37c6856f6c6bacdf04bdb920b1bd25794c1561a1
nikita <jadhavniku5o@gmail.com>
2023-01-18 21:58:47 +0530

    HashSet

--- src/com/collectiontest/HashSetDemo.java
+++ src/com/collectiontest/HashSetDemo.java
@@ -1,5 +1,19 @@
 package com.collectiontest;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class HashSetDemo {
+	   
+	public static void main(String[] args) {
+		
+		HashSet<Integer> set = new HashSet<Integer>();
+		set.add(1);
+		set.add(2);
+		set.add(3);
+		set.add(4);
+		set.add(5);
+		System.out.println(set);
+	}
 
 }
