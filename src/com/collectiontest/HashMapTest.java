594558076e6ef0d612a27308dcff4c8cb47a2e01
nikita <jadhavniku5o@gmail.com>
2023-01-19 12:17:29 +0530

    New class

---
+++ src/com/collectiontest/HashMapTest.java
@@ -0,0 +1,19 @@
+package com.collectiontest;
+
+import java.util.HashMap;
+
+public class HashMapTest {
+	
+public static void main(String[] args) {
+		
+		HashMap hm = new HashMap();
+		hm.put("nikita",12);
+		hm.put("mayur",18);
+		hm.put("abhi",65);
+		hm.put("ram",34);
+		
+		System.out.println(hm);
+	}
+
+
+}
